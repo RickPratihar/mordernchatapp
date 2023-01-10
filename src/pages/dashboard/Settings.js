bd7768a883a953931e59cc7227e662feb6ab8c91
rick7890 <rickpratihar2@gmail.com>
2023-01-10 19:10:10 +0530

    change

---
+++ src/pages/dashboard/Settings.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { Stack, Box, IconButton, Typography, Avatar, Divider } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { Bell, Lock, Key, PencilCircle, Image, Note, Keyboard, Info, CaretLeft } from "phosphor-react";
+import { faker } from "@faker-js/faker";
+import Shortcuts from "../../Sections/settings/Shortcuts";
+
+
+const Settings = () => {
+
+    const theme = useTheme();
+
+     const [openShortcuts, setOpenShortcuts] = useState(false);
+
+  const handleOpenShortcuts = () => {
+    setOpenShortcuts(true);
+  };
+
+  const handleCloseShortcuts = () => {
+    setOpenShortcuts(false);
+  };
+   
+
+    const list = [
+        {
+            key: 0,
+            icon: <Bell size={20} />,
+            title: "Notifications",
+            onclick: () => { },
+        },
+        {
+            key: 1,
+            icon: <Lock size={20} />,
+            title: "Privacy",
+            onclick: () => { },
+        },
+        {
+            key: 2,
+            icon: <Key size={20} />,
+            title: "Security",
+            onclick: () => { },
+        },
+        {
+            key: 3,
+            icon: <PencilCircle size={20} />,
+            title: "Theme",
+            //onclick: handleOpenTheme,
+        },
+        {
+            key: 4,
+            icon: <Image size={20} />,
+            title: "Chat Wallpaper",
+            onclick: () => { },
+        },
+        {
+            key: 5,
+            icon: <Note size={20} />,
+            title: "Request Account Info",
+            onclick: () => { },
+        },
+        {
+            key: 6,
+            icon: <Keyboard size={20} />,
+            title: "Keyboard Shortcuts",
+            onclick: handleOpenShortcuts,
+            // onclick: () => { },
+        },
+        {
+            key: 7,
+            icon: <Info size={20} />,
+            title: "Help",
+            onclick: () => { },
+        },
+    ];
+
+    return (
+        <>
+            <Stack direction={"row"} sx={{ width: "100%" }}>
+                {/*leftPanel*/}
+                <Box
+                    sx={{
+                        overflowY: "",
+                        height: "100vh",
+                        width: 320,
+                        backgroundColor:
+                            theme.palette.mode === "light"
+                                ? "#F8FAFF"
+                                : theme.palette.background,
+                        boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)"
+                    }}>
+                    <Stack p={4} spacing={5}>
+                        {/*Header*/}
+                        <Stack direction={"row"} alignItems="center" spacing={3}>
+                            <IconButton>
+                                <CaretLeft size={24} color={"#4B4B"} />
+                            </IconButton>
+                            <Typography variant="h6">
+                                Settings
+                            </Typography>
+                        </Stack>
+
+                        {/*Profile*/}
+                        <Stack direction={'row'} spacing={3}>
+                            <Avatar sx={{ width: 56, height: 56 }} src={faker.image.avatar()} alt={faker.name.fullName} />
+                            <Stack spacing={0.5}>
+                                <Typography variant="article">
+                                    {faker.name.fullName}
+                                </Typography>
+                                <Typography variant="article">
+                                    {faker.random.words()}
+                                </Typography>
+
+                            </Stack>
+                        </Stack>
+                        {/*List of option*/}
+                        <Stack spacing={4}>
+                            {list.map(({ key, icon, title, onclick }) => {
+                                return (
+                                    <>
+                                        <Stack
+                                            onClick={onclick}
+                                            sx={{ cursor: "pointer" }}
+                                            spacing={2}
+                                        >
+                                            <Stack alignItems={"center"} direction="row" spacing={2}>
+                                                {icon}
+                                                <Typography variant="body2">{title}</Typography>
+                                            </Stack>
+                                            {key !== 7 && <Divider />}
+                                        </Stack>
+                                    </>
+                                );
+                            })}
+                        </Stack>
+                    </Stack>
+
+                </Box>
+                {/*RightPanel*/}
+            </Stack>
+            {openShortcuts &&  <Shortcuts open={openShortcuts} handleClose={handleCloseShortcuts} /> }
+        </>
+    )
+}
+
+
+export default Settings;
